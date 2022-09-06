59ff1bb3f145cdd8fa6e3cfb5b52905c073b4a7c
Julia Hoi <j.x1nyi2@gmail.com>
2022-09-06 16:08:13 +0200

    Add libraries and code

---
+++ index.js
@@ -0,0 +1,30 @@
+import { argv } from 'node:process';
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+let colorCode = randomColor();
+const hashString = `###############################
+###############################
+###############################
+#####                     #####
+#####       ${colorCode}       #####
+#####                     #####
+###############################
+###############################
+###############################`;
+
+if (argv[2] === 'dark' && argv[3]) {
+  let colorCode = randomColor({
+    luminosity: 'dark',
+    hue: argv[3],
+  });
+  console.log(chalk.hex(`${colorCode}`)(`${hashString}`));
+} else if (argv[2] === 'light') {
+  let colorCode = randomColor({
+    luminosity: 'light',
+    hue: argv[3],
+  });
+  console.log(chalk.hex(`${colorCode}`)(`${hashString}`));
+} else {
+  console.log(chalk.hex(`${colorCode}`)(`${hashString}`));
+}
