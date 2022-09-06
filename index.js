0a06c33e56f22a29266df021ce60b416bd2247bc
Julia Hoi <j.x1nyi2@gmail.com>
2022-09-06 21:19:04 +0200

    Update ask

--- index.js
+++ index.js
@@ -13,16 +13,18 @@ const hashString = `###############################
 ###############################
 ###############################`;
 
-if (argv[2] === 'dark' && argv[3]) {
+if (argv[2] === 'ask') {
+  console.log('What hue and luminosity would you me to generate?');
+} else if (argv[2] && argv[3] === 'dark') {
   colorCode = randomColor({
     luminosity: 'dark',
-    hue: argv[3],
+    hue: argv[2],
   });
   console.log(chalk.hex(colorCode)(hashString));
-} else if (argv[2] === 'light' && argv[3]) {
+} else if (argv[2] && argv[3] === 'light') {
   colorCode = randomColor({
     luminosity: 'light',
-    hue: argv[3],
+    hue: argv[2],
   });
   console.log(chalk.hex(colorCode)(hashString));
 } else if (argv[2]) {
