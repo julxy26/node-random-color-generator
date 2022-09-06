0ee1e5794745691440a47cccb0713773c9d05b73
Julia Hoi <j.x1nyi2@gmail.com>
2022-09-06 21:46:18 +0200

    Update correct ask

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const hashString = `###############################
 ###############################`;
 
 if (argv[2] === 'ask') {
-  console.log('What hue and luminosity would you me to generate?');
+  console.log('What hue and luminosity would you like me to generate?');
 } else if (argv[2] && argv[3] === 'dark') {
   colorCode = randomColor({
     luminosity: 'dark',
