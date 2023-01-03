1d956d76b876c0a9887d87b62b9eed474258ccc4
Julia Hoi <j.x1nyi2@gmail.com>
2023-01-03 23:35:57 +0100

    Fix code and update loop

--- index.js
+++ index.js
@@ -4,33 +4,50 @@ import randomColor from 'randomcolor';
 
 let colorCode = randomColor();
 
-const hashString = `###############################
-###############################
-###############################
-#####                     #####
-#####       ${colorCode}       #####
-#####                     #####
-###############################
-###############################
-###############################`;
+// const hashString = `###############################
+// ###############################
+// ###############################
+// #####                     #####
+// #####       ${colorCode}       #####
+// #####                     #####
+// ###############################
+// ###############################
+// ###############################`;
 
-if (argv[2] === 'ask') {
-  console.log('What hue and luminosity would you like me to generate?');
-} else if (argv[2] && argv[3] === 'dark') {
-  colorCode = randomColor({
-    luminosity: 'dark',
-    hue: argv[2],
-  });
-} else if (argv[2] && argv[3] === 'light') {
-  colorCode = randomColor({
-    luminosity: 'light',
-    hue: argv[2],
-  });
-} else if (argv[2]) {
-  colorCode = randomColor({
-    hue: argv[2],
-  });
-} else {
+const width = 31;
+const height = 9;
+const hashRow = ('#'.repeat(width) + '\n').repeat(3);
+const hashGap = '#'.repeat(4) + ' '.repeat(width - 8) + '#'.repeat(4) + '\n';
+const hashColorCode =
+  '#'.repeat(height / 3 + 1) +
+  ' '.repeat(8) +
+  colorCode +
+  ' '.repeat(8) +
+  '#'.repeat(height / 3 + 1) +
+  '\n';
+
+const hashString = hashRow + hashGap + hashColorCode + hashGap + hashRow;
+
+function generateColor() {
+  if (argv[2] === 'ask') {
+    console.log('What luminosity and hue would you like me to generate?');
+    return;
+  } else if (argv[2] === 'dark') {
+    colorCode = randomColor({
+      luminosity: 'dark',
+      hue: argv[3],
+    });
+  } else if (argv[2] === 'light') {
+    colorCode = randomColor({
+      luminosity: 'light',
+      hue: argv[3],
+    });
+  } else {
+    colorCode = randomColor({
+      hue: argv[2],
+    });
+  }
+  console.log(chalk.hex(colorCode)(hashString));
 }
 
-console.log(chalk.hex(colorCode)(hashString));
+generateColor();
